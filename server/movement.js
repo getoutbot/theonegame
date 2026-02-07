ae0c306d310a104ea8a69ae9506f104d23c59f82
getoutbot <ilikeanimehehe4@gmail.com>
2026-02-07 14:27:59 -0700

    Initialize movement mechanics for parkour

---
+++ server/movement.js
@@ -0,0 +1,21 @@
+// Movement mechanics including wall-running, double jumping, and sliding
+
+class Movement {
+    constructor(player) {
+        this.player = player;
+    }
+
+    wallRun() {
+        // wall-running logic
+    }
+
+    doubleJump() {
+        // double jump logic
+    }
+
+    slide() {
+        // sliding logic
+    }
+}
+
+module.exports = Movement;
